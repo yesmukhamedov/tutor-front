e50db9b1cbaa6b1e9c79d8b1ef3a3285acd3227b
yesmukhamedov <yesmukhamedov.yeskendyr@gmail.com>
2023-09-21 08:09:04 +0600

    upgrade react react-dom version

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
-//import ReactDOM from "react-dom/client";
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom/client";
 
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
@@ -9,20 +8,12 @@ import 'antd/dist/antd.min.css';
 import store from './redux/store';
 import App from './App';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <Router>
     <Provider store={store}>
       <App />
     </Provider>
   </Router>
-, document.getElementById('root'));
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// root.render(
-//   <Router>
-//     <Provider store={store}>
-//       <App />
-//     </Provider>
-//   </Router>
-// );
+);
